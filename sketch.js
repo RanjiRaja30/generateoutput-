18b13bcfdfa0f83d0e695b1dcccf0160b1da4ba8
RanjiRaja30 <75240366+RanjiRaja30@users.noreply.github.com>
2022-07-10 15:50:23 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+
+var trex ,trex_running;
+function preload(){
+  
+
+}
+
+function setup(){
+  createCanvas(600,200)
+ 
+  //create a trex sprite
+  trex = createSprite(50,180,20,50);
+  var ranji=10;
+  console.log(ranji);
+}
+
+function draw(){
+  background("white")
+
+
+  console.log("arnav"+123456789);
+  drawSprites();
+}
